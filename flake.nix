3888299c20a00f9c992b83ff2f5ea3b4bf74cb57
winston <hey@winston.sh>
2023-05-12 22:41:17 +0200

    build(nix): add macOS derivation, use april-asr flake
    Co-authored-by: Carsten Kragelund <carsten@kragelund.me>

--- flake.nix
+++ flake.nix
@@ -3,26 +3,45 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    april-asr = {
-      url = "https://april.sapples.net/aprilv0_en-us.april";
+    nixpkgs-onnxruntime114.url = "github:nixos/nixpkgs/c7d48290f9da479dcab26eac5db6299739c595c2";
+    # TODO: replace with upstreamed version on abb128
+    april-asr.url = "github:nekowinston/april-asr/d032c6eebbff960febff50f4c56d1e1420b1b318";
+    april-asr-model = {
+      url = "https://april.sapples.net/april-english-dev-01110_en.april";
       flake = false;
     };
   };
   outputs = {
     self,
     april-asr,
+    april-asr-model,
     nixpkgs,
+    nixpkgs-onnxruntime114,
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            # TODO: remove once https://github.com/NixOS/nixpkgs/pull/226734 is merged
+            (
+              final: prev: {
+                onnxruntime = nixpkgs-onnxruntime114.legacyPackages.${prev.system}.onnxruntime;
+                aprilasr = april-asr.packages.${prev.system}.april-asr;
+              }
+            )
+          ];
+        };
+        inherit (pkgs) lib;
+        inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
       in {
         packages = rec {
-          livecaptions = pkgs.stdenv.mkDerivation rec {
+          livecaptions = pkgs.stdenv.mkDerivation {
             src = ./.;
             name = "livecaptions";
             nativeBuildInputs = with pkgs; [
+              makeWrapper
               meson
               ninja
               wrapGAppsHook
@@ -37,15 +56,24 @@
               libadwaita # libadwaita-1
               libpulseaudio # libpulse
               onnxruntime
+              aprilasr
             ];
-            preFixup = ''
-              gappsWrapperArgs+=(--prefix APRIL_MODEL_PATH : "${april-asr}")
+            patchPhase = ''
+              # remove lines containing april
+              sed -i "/april/d" meson.build
+              sed -i "s/april_lib/dependency('aprilasr')/" src/meson.build
             '';
-            meta = with pkgs.lib; {
+            preFixup =
+              lib.optionalString isLinux ''
+                gappsWrapperArgs+=(--prefix APRIL_MODEL_PATH : "${april-asr-model}")
+              ''
+              + lib.optionalString isDarwin ''
+                wrapProgram "$out/bin/livecaptions" --prefix APRIL_MODEL_PATH : "${april-asr-model}"
+              '';
+            meta = with lib; {
               description = "Linux Desktop application that provides live captioning";
               homepage = "https://github.com/abb128/LiveCaptions";
               license = licenses.gpl3;
-              platforms = platforms.linux;
             };
           };
           default = livecaptions;
@@ -58,7 +86,7 @@
           inherit (self.packages.${system}.livecaptions) buildInputs nativeBuildInputs;
           shellHook = ''
             export LD_LIBRARY_PATH=${pkgs.onnxruntime}/lib
-            export APRIL_MODEL_PATH=${april-asr}
+            export APRIL_MODEL_PATH=${april-asr-model}
           '';
         };
       }
